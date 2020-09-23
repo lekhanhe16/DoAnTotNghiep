1ef2c922c86b615ab7df30b00a87fa2a61cafcc1
lekhanhe16ptit <lekhanhe16ptit@gmail.com>
2020-09-23 10:28:42 +0700

    update

--- age_gender/predict.py
+++ age_gender/predict.py
@@ -1,32 +1,26 @@
 # import argparse
 # import os
 import base64
-import io, os
+import io
+import os
 
 import cv2
+import mxnet as mx
 import numpy as np
 from PIL import Image
-from keras.models import load_model
-from keras.preprocessing import image
-from age_gender.utils.datasets import get_labels
-from age_gender.utils.preprocessor import preprocess_input
-from age_gender.utils.inference import load_detection_model
-from age_gender.utils.inference import detect_faces
-from age_gender.utils.inference import apply_offsets
 from insightface import model_zoo
 from insightface.utils import face_align
-import mxnet as mx
+from keras.models import load_model
+from keras.preprocessing import image
+from tensorflow.python.keras.backend import set_session
+import tensorflow as tf
+from age_gender.utils.datasets import get_labels
+from age_gender.utils.preprocessor import preprocess_input
 
-detection_model_path = './age_gender/detection_models/haarcascade_frontalface_default.xml'
-face_detection = load_detection_model(detection_model_path)
-
-emotion_model_path = './age_gender/emotion_models/fer2013_mini_XCEPTION.102-0.66.hdf5'
-emotion_labels = get_labels('fer2013')
-emotion_classifier = load_model(emotion_model_path, compile=False)
-emotion_target_size = emotion_classifier.input_shape[1:3]
-frame_window = 10
-emotion_offsets = (20, 40)
-emotion_offsets = (0, 0)
+sess = tf.Session()
+graph = tf.get_default_graph()
+set_session(sess)
+# saved_model = tf.keras.models.load_model("test.h5")
 
 model_str = './age_gender/models/ssr2_megaage_1_1/model,0'
 model_gender_str = './age_gender/models/ssr2_imdb_gender_1_1/model,0'
@@ -49,37 +43,50 @@ else:
     ctx = 0
 ga_model.prepare(ctx_id)
 
+# detection_model_path = 'haarcascade_frontalface_default.xml'
+# face_detection = load_detection_model(detection_model_path)
+emotion_model_path = './age_gender/emotion_models/fer2013_mini_XCEPTION.102-0.66.hdf5'
+emotion_labels = get_labels('fer2013')
+emotion_classifier = load_model(emotion_model_path, compile=False)
+emotion_target_size = emotion_classifier.input_shape[1:3]
+frame_window = 10
+emotion_offsets = (20, 40)
+emotion_offsets = (0, 0)
+
 #######RSS model
 # rss_model_age = get_model(ctx,(64,64),model_str,'_mulscalar16')
-_vec = model_str.split(',')
-assert len(_vec) == 2
-prefix = _vec[0]
-epoch = int(_vec[1])
-print('loading', prefix, epoch)
-sym, arg_params, aux_params = mx.model.load_checkpoint(prefix, epoch)
-all_layers = sym.get_internals()
-sym = all_layers['_mulscalar16' + '_output']
-rss_model_age = mx.mod.Module(symbol=sym, data_names=('data', 'stage_num0', 'stage_num1', 'stage_num2'),
+_vec1 = model_str.split(',')
+assert len(_vec1) == 2
+prefix1 = _vec1[0]
+epoch1 = int(_vec1[1])
+# print('loading', prefix1, epoch1)
+sym1, arg_params1, aux_params1 = mx.model.load_checkpoint(prefix1, epoch1)
+all_layers1 = sym1.get_internals()
+
+sym1 = all_layers1['_mulscalar16' + '_output']
+rss_model_age = mx.mod.Module(symbol=sym1, data_names=('data', 'stage_num0', 'stage_num1', 'stage_num2'),
                               context=ctx_context, label_names=None)
 rss_model_age.bind(
-    data_shapes=[('data', (1, 3, 64, 64)), ('stage_num0', (1, 3)), ('stage_num1', (1, 3)), ('stage_num2', (1, 3))])
-rss_model_age.set_params(arg_params, aux_params)
+    data_shapes=[('data', (1, 3, 64, 64)), ('stage_num0', (1, 3)), ('stage_num1', (1, 3)),
+                 ('stage_num2', (1, 3))])
+rss_model_age.set_params(arg_params1, aux_params1)
 
 ##### rss gender #####
 # rss_model_gender = get_model_gender(ctx,(64,64),model_gender_str,'_mulscalar16')
-_vec = model_gender_str.split(',')
-assert len(_vec) == 2
-prefix = _vec[0]
-epoch = int(_vec[1])
-print('loading', prefix, epoch)
-sym, arg_params, aux_params = mx.model.load_checkpoint(prefix, epoch)
-all_layers = sym.get_internals()
-sym = all_layers['_mulscalar16' + '_output']
-rss_model_gender = mx.mod.Module(symbol=sym, data_names=('data', 'stage_num0', 'stage_num1', 'stage_num2'),
+_vec2 = model_gender_str.split(',')
+assert len(_vec2) == 2
+prefix2 = _vec2[0]
+epoch2 = int(_vec2[1])
+# print('loading', prefix2, epoch2)
+sym2, arg_params2, aux_params2 = mx.model.load_checkpoint(prefix2, epoch2)
+all_layers2 = sym2.get_internals()
+sym2 = all_layers2['_mulscalar16' + '_output']
+rss_model_gender = mx.mod.Module(symbol=sym2, data_names=('data', 'stage_num0', 'stage_num1', 'stage_num2'),
                                  context=ctx_context, label_names=None)
 rss_model_gender.bind(
-    data_shapes=[('data', (1, 3, 64, 64)), ('stage_num0', (1, 3)), ('stage_num1', (1, 3)), ('stage_num2', (1, 3))])
-rss_model_gender.set_params(arg_params, aux_params)
+    data_shapes=[('data', (1, 3, 64, 64)), ('stage_num0', (1, 3)), ('stage_num1', (1, 3)),
+                 ('stage_num2', (1, 3))])
+rss_model_gender.set_params(arg_params2, aux_params2)
 
 
 def predict_emotion(base64_image):
@@ -97,22 +104,22 @@ def predict_emotion(base64_image):
         gray_frame = image.img_to_array(gray_frame)
         gray_frame = np.squeeze(gray_frame)
         gray_frame = gray_frame.astype('uint8')
-        faces = detect_faces(face_detection, gray_frame)
+        # faces = detect_faces(face_detection, gray_frame)
 
-        for face_coordinates in faces:
+        # for face_coordinates in faces:
 
-            x1, x2, y1, y2 = apply_offsets(face_coordinates, emotion_offsets)
-            gray_face = gray_frame[y1:y2, x1:x2]
-            try:
-                gray_face = cv2.resize(gray_face, (emotion_target_size))
-            except:
-                continue
-            gray_face = preprocess_input(gray_face, True)
-            gray_face = np.expand_dims(gray_face, 0)
-            gray_face = np.expand_dims(gray_face, -1)
-            emotion_prediction = emotion_classifier.predict(gray_face)
-            emotion_label_arg = np.argmax(emotion_prediction)
-            emotion_text.append(emotion_labels[emotion_label_arg])
+        # x1, x2, y1, y2 = apply_offsets(gray_frame, emotion_offsets)
+        # gray_face = gray_frame[y1:y2, x1:x2]
+        # try:
+        gray_face = cv2.resize(gray_frame, emotion_target_size)
+        # except:
+
+        gray_face = preprocess_input(gray_face, True)
+        gray_face = np.expand_dims(gray_face, 0)
+        gray_face = np.expand_dims(gray_face, -1)
+        emotion_prediction = emotion_classifier.predict(gray_face)
+        emotion_label_arg = np.argmax(emotion_prediction)
+        emotion_text.append(emotion_labels[emotion_label_arg])
 
         if len(emotion_text) == 0:
             emotion_text = [' ']
@@ -123,56 +130,61 @@ def predict_emotion(base64_image):
         return [' ']
 
 
-def predict_ga(base64_image):
-    file_like = io.BytesIO(base64.b64decode(base64_image))
-    image_input = Image.open(file_like)
-    frame = np.array(image_input)
-    # frame = cv2.cvtColor(frame_1, cv2.COLOR_BGR2RGB)
-    try:
-        bboxes, landmarks = det_model.detect(frame, threshold=0.8, scale=1.0)
-        if len(landmarks) > 0:
-            _img = face_align.norm_crop(frame, landmark=landmarks[0])
-            if _img is None:
-                gender, age = None, None
-            else:
-                gender, age = ga_model.get(_img)
-            print('FaceAgeOnly', gender, age)
-        else:
-            gender, age = None, None
+def predict(base64_image):
+    global sess
+    global graph
+    with graph.as_default():
+        set_session(sess)
+        file_like = io.BytesIO(base64.b64decode(base64_image))
+        image_input = Image.open(file_like)
+        frame = np.array(image_input)
+        # frame = cv2.cvtColor(frame_1, cv2.COLOR_BGR2RGB)
 
-        rss_input = cv2.cvtColor(_img, cv2.COLOR_BGR2RGB)
-        rss_input = cv2.resize(rss_input, (64, 64))
-        nimg = rss_input[:, :, ::-1]
-        nimg = np.transpose(nimg, (2, 0, 1))
-
-        input_blob = np.expand_dims(nimg, axis=0)
-        data = mx.nd.array(input_blob)
-        db = mx.io.DataBatch(data=(data, mx.nd.array([[0, 1, 2]]), mx.nd.array([[0, 1, 2]]), mx.nd.array([[0, 1, 2]])))
-        rss_model_age.forward(db, is_train=False)
-        age_1 = rss_model_age.get_outputs()[0].asnumpy()
-
-        rss_model_gender.forward(db, is_train=False)
-        gender_1 = rss_model_gender.get_outputs()[0].asnumpy()
-
-        if len(age_1) > 0:
-            age_1 = age_1[0]
-        if len(gender_1) > 0:
-            gender_1 = gender_1[0]
-
-        print('rss ', gender_1, age_1)
-
-        if gender is not None and len(gender_1) > 0:
-            if gender != round(gender_1[0], 0):
-                if gender_1[0] > 0:
-                    gender = 1
+        try:
+            bboxes, landmarks = det_model.detect(frame, threshold=0.8, scale=1.0)
+            if len(landmarks) > 0:
+                _img = face_align.norm_crop(frame, landmark=landmarks[0])
+                if _img is None:
+                    gender, age = None, None
                 else:
-                    gender = 0
-                # return gender_1, age, emotion_text
-        if age is not None and len(age_1) > 0:
-            age = round((0.5 * age + 0.5 * age_1[0]), 0)
+                    gender, age = ga_model.get(_img)
+                # print('FaceAgeOnly', gender, age)
+            else:
+                gender, age = None, None
 
-        return gender, age
-    except Exception as e:
-        print(e)
-        return None, None
+            rss_input = cv2.cvtColor(_img, cv2.COLOR_BGR2RGB)
+            rss_input = cv2.resize(rss_input, (64, 64))
+            nimg = rss_input[:, :, ::-1]
+            nimg = np.transpose(nimg, (2, 0, 1))
 
+            input_blob = np.expand_dims(nimg, axis=0)
+            data = mx.nd.array(input_blob)
+            db = mx.io.DataBatch(
+                data=(data, mx.nd.array([[0, 1, 2]]), mx.nd.array([[0, 1, 2]]), mx.nd.array([[0, 1, 2]])))
+            rss_model_age.forward(db, is_train=False)
+            age_1 = rss_model_age.get_outputs()[0].asnumpy()
+
+            rss_model_gender.forward(db, is_train=False)
+            gender_1 = rss_model_gender.get_outputs()[0].asnumpy()
+
+            if len(age_1) > 0:
+                age_1 = age_1[0]
+            if len(gender_1) > 0:
+                gender_1 = gender_1[0]
+
+            # print('rss ', gender_1, age_1)
+
+            if gender is not None and len(gender_1) > 0:
+                if gender != round(gender_1[0], 0):
+                    if gender_1[0] > 0:
+                        gender = 1
+                    else:
+                        gender = 0
+
+            if age is not None and len(age_1) > 0:
+                age = round((0.5 * age + 0.5 * age_1[0]), 0)
+
+            return gender, age
+        except Exception as e:
+            print(e)
+            return None, None
--- index.py
+++ index.py
@@ -14,143 +14,153 @@ from logging.handlers import RotatingFileHandler
 import logging
 from time import strftime
 import traceback
-from age_gender import predict
+
 from gevent.pywsgi import WSGIServer
 
 import cv2
 import base64
+import requests
+import numpy as np
+from requests.auth import HTTPBasicAuth
+
 import io
+
+from age_gender.predict import predict, predict_emotion
 
 # from age_gender.predict import predistr(gender) + '_' + str(age)ct
 
-handler = RotatingFileHandler(filename=os.path.join('logs', 'app.log'), maxBytes=1024 * 1024 * 1, backupCount=10)
-logger = logging.getLogger('tdm')
-logger.setLevel(logging.DEBUG)
-logger.addHandler(handler)
-
-# config server
-app = Flask(__name__)
-
-
-# Log server
-@app.after_request
-def after_request(response):
-    timestamp = strftime('[%Y-%b-%d %H:%M:%S]')
-    logger.error('%s %s %s %s %s %s', timestamp, request.remote_addr, request.method, request.scheme, request.full_path,
-                 response.status)
-    return response
-
-
-@app.errorhandler(Exception)
-def exceptions(e):
-    tb = traceback.format_exc()
-    timestamp = strftime('[%Y-%b-%d %H:%M:%S]')
-    logger.error('%s %s %s %s %s 5xx INTERNAL SERVER ERROR\n%s', timestamp, request.remote_addr, request.method,
-                 request.scheme, request.full_path, tb)
-    return 0
-
-
-CORS(app, allow_headers=['Authorization', 'Content-Type'])
-
-
-# handle http
-@app.route("/")
-def index():
-    return "Hello beetsoft!"
-
-
-# static filename
-
-@app.after_request
-def add_header(r):
-    """
-    Add headers to both force latest IE rendering engine or Chrome Frame,
-    and also to cache the rendered page for 10 minutes.
-    """
-    r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
-    r.headers["Pragma"] = "no-cache"
-    r.headers["Expires"] = "0"
-    r.headers['Cache-Control'] = 'public, max-age=0'
-    return r
-
-
-@app.route("/api/get_emotion", methods=['GET', 'POST'])
-def get_emotion():
-    request_json = request.get_json()
-    try:
-        image = request_json.get('image')
-        _emotion = predict.predict_emotion(image)
-        if len(_emotion) > 0:
-            _emotion = _emotion[0]
-        else:
-            _emotion = []
-        emotion = ''
-
-        if _emotion in ['sad', 'angry', 'disgust', 'fear']:
-            emotion = 'sad'
-        if _emotion in ['happy', 'surprise']:
-            emotion = 'happy'
-        if _emotion == 'neutral':
-            emotion = 'neutral'
-        data = {'emotion': emotion}
-        print(data)
-        return jsonify({'status': 10000, 'data': data})
-    except Exception as e:
-        print(e)
-        return jsonify({'status': 10002})
-
-
+# handler = RotatingFileHandler(filename=os.path.join('logs', 'app.log'), maxBytes=1024 * 1024 * 1, backupCount=10)
+# logger = logging.getLogger('tdm')
+# logger.setLevel(logging.DEBUG)
+# logger.addHandler(handler)
+#
+# # config server
+# app = Flask(__name__)
+#
+#
+# # Log server
+# @app.after_request
+# def after_request(response):
+#     timestamp = strftime('[%Y-%b-%d %H:%M:%S]')
+#     logger.error('%s %s %s %s %s %s', timestamp, request.remote_addr, request.method, request.scheme, request.full_path,
+#                  response.status)
+#     return response
+#
+#
+# @app.errorhandler(Exception)
+# def exceptions(e):
+#     tb = traceback.format_exc()
+#     timestamp = strftime('[%Y-%b-%d %H:%M:%S]')
+#     logger.error('%s %s %s %s %s 5xx INTERNAL SERVER ERROR\n%s', timestamp, request.remote_addr, request.method,
+#                  request.scheme, request.full_path, tb)
+#     return 0
+#
+#
+# CORS(app, allow_headers=['Authorization', 'Content-Type'])
+#
+#
+# # handle http
+# @app.route("/")
+# def index():
+#     return "Hello beetsoft!"
+#
+#
+# # static filename
+#
+# @app.after_request
+# def add_header(r):
+#     """
+#     Add headers to both force latest IE rendering engine or Chrome Frame,
+#     and also to cache the rendered page for 10 minutes.
+#     """
+#     r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+#     r.headers["Pragma"] = "no-cache"
+#     r.headers["Expires"] = "0"
+#     r.headers['Cache-Control'] = 'public, max-age=0'
+#     return r
+#
+#
+# @app.route("/api/get_emotion", methods=['GET', 'POST'])
+# def get_emotion():
+#     request_json = request.get_json()
+#     try:
+#         image = request_json.get('image')
+#         _emotion = predict_emotion(image)
+#         if len(_emotion) > 0:
+#             _emotion = _emotion[0]
+#         else:
+#             _emotion = []
+#         emotion = ''
+#
+#         if _emotion in ['sad', 'angry', 'disgust', 'fear']:
+#             emotion = 'sad'
+#         if _emotion in ['happy', 'surprise']:
+#             emotion = 'happy'
+#         if _emotion == 'neutral':
+#             emotion = 'neutral'
+#         data = {'emotion': emotion}
+#         print(data)
+#         return jsonify({'status': 10000, 'data': data})
+#     except Exception as e:
+#         print(e)
+#         return jsonify({'status': 10002})
+#
+url = "http://ipcampython:1234567@192.168.42.129:8080/video"
 if __name__ == '__main__':
     # app.run(host='0.0.0.0', debug=False, port=3000)
     # http_server = WSGIServer(('0.0.0.0', 3000), app)
     # http_server.serve_forever()
+
     face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+    # video_capture = cv2.VideoCapture(url)
     video_capture = cv2.VideoCapture(0)
-
-    process_this_frame = True
-
-    i = 0
     try:
+
         while True:
             # Grab a single frame of video
             ret, frame = video_capture.read()
-
+            fps = video_capture.get(cv2.CAP_PROP_FPS)
             # Resize frame of video to 1/4 size for faster face recognition processing
             small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
-
             gray = cv2.cvtColor(small_frame, cv2.COLOR_BGR2GRAY)
-            faces = face_cascade.detectMultiScale(gray, 1.1, 4)
-            for face in faces:
-                # cv2.rectangle(frame, (face[0], face[1]), (face[0] + face[2], face[1] + face[3]), (0, 255, 0), 2)
-                # if process_this_frame:
-                detected_face = small_frame[face[1]: (face[1] + face[3]), face[0]:(face[0] + face[2])]
-                ret, jpeg = cv2.imencode('.jpg', detected_face)
-                imgdata = jpeg.tobytes()
+            faces = face_cascade.detectMultiScale(gray, 1.1, 5)
 
-                _emotion = predict.predict_emotion(str(base64.b64encode(imgdata).decode('utf-8')))
-                if len(_emotion) > 0:
-                    _emotion = _emotion[0]
-                else:
-                    _emotion = []
-                emotion = 'None'
+            if (len(faces) > 0):
+                for face in faces:
+                    detected_face = small_frame[face[1]: (face[1] + face[3]), face[0]:(face[0] + face[2])]
+                    ret, jpeg = cv2.imencode('.jpg', detected_face)
+                    imgdata = jpeg.tobytes()
 
-                if _emotion in ['sad', 'angry', 'disgust', 'fear']:
-                    emotion = 'sad'
-                if _emotion in ['happy', 'supprise']:
-                    emotion = 'happy'
-                if _emotion == 'neutral':
-                    emotion = 'neutral'
+                    age = ''
+                    gender = ''
+                    _emotion = predict_emotion(str(base64.b64encode(imgdata).decode('utf-8')))
+                    if len(_emotion) > 0:
+                        _emotion = _emotion[0]
+                    else:
+                        _emotion = []
+                    emotion = 'None'
 
-                gender, age = predict.predict_ga(str(base64.b64encode(imgdata).decode('utf-8')))
+                    if _emotion in ['sad', 'angry', 'disgust', 'fear']:
+                        emotion = 'sad'
+                    if _emotion in ['happy', 'supprise']:
+                        emotion = 'happy'
+                    if _emotion == 'neutral':
+                        emotion = 'neutral'
+                        # ret2, jpeg2 = cv2.imencode('.jpg', small_frame)
+                        # imgdata2 = jpeg2.tobytes()
+                        gender, age = predict(str(base64.b64encode(imgdata).decode('utf-8')))
 
-                cv2.putText(frame, emotion + '_' + str(age) + '_' + str(gender),
-                            (int(face[0] / 0.25), int(face[1] / 0.25)), cv2.FONT_HERSHEY_DUPLEX, 1.0,
-                            (0, 0, 255), 1)
-
+                    cv2.putText(frame, emotion + '_' + str(age) + '_' + str(gender),
+                                (int(face[0] / 0.25), int(face[1] / 0.25)), cv2.FONT_HERSHEY_DUPLEX, 0.75,
+                                (0, 0, 255), 1)
+                    cv2.putText(frame, str(fps),
+                                (20, 20), cv2.FONT_HERSHEY_DUPLEX, 0.75,
+                                (0, 0, 255), 1)
+                    # cv2.resizeWindow()
                 cv2.imshow("Video", frame)
-
+            else:
+                cv2.imshow("Video", frame)
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
-
     except Exception as e:
         print(e)
